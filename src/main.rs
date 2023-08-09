79407278ba0555ef8f327a92df5d2de40be70b5c
Cabidge <cabbageboi69@gmail.com>
2023-08-09 17:45:51 -0400

    set up maud

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+use maud::{html, Markup};
+
 #[tokio::main]
 async fn main() {
     use axum::routing::get;
 
-    let app = axum::Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = axum::Router::new().route("/", get(root));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn root() -> Markup {
+    render_base(
+        "Hello, World",
+        html! {
+            h1 { "Hello, World!" }
+        },
+    )
+}
+
+fn render_base(title: &str, body: Markup) -> Markup {
+    html! {
+        html {
+            head {
+                title { (title) }
+            }
+            body {
+                (body)
+            }
+        }
+    }
 }
